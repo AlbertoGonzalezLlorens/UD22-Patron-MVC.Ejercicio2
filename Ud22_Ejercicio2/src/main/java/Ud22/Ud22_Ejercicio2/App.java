e2ad8c8cf7834671222a822bb63d3100b29d2cb4
AlbertoGonzalezLlorens <114727348+AlbertoGonzalezLlorens@users.noreply.github.com>
2022-11-08 11:36:03 +0100

    Proyecto Maven

---
+++ Ud22_Ejercicio2/src/main/java/Ud22/Ud22_Ejercicio2/App.java
@@ -0,0 +1,13 @@
+package Ud22.Ud22_Ejercicio2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Ud22_Ejercicio2/src/test/java/Ud22/Ud22_Ejercicio2/AppTest.java
@@ -0,0 +1,20 @@
+package Ud22.Ud22_Ejercicio2;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
